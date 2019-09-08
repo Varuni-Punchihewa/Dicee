ec0a090e01bcdeaed9a4c9196d20ad41f5f71bc1
Varuni-Punchihewa <1995varuni@gmail.com>
2019-09-08 22:43:31 +0530

    add randomly generated dicee

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,64 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue[600],
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text(
+            'Dicee',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.blue[100],
         ),
         body: DicePage(),
       ),
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNumber = 1;
+  int rightNumber = 1;
+
+  numberGenerator() {
+    setState(() {
+      leftNumber = Random().nextInt(6) + 1;
+      rightNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftNumber.png'),
+              onPressed: () {
+                numberGenerator();
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightNumber.png'),
+              onPressed: () {
+                numberGenerator();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
